f2d5f40cce95c5032a988b0685ab3c8322c9f061
Suxarik777 <gultcev@mail.ru>
2022-08-04 22:54:51 +0300

    1 Семинар

---
+++ Seminar_1/Example002/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+
+
+Console.WriteLine("Мы сгенерировали два числа: ");
+Console.WriteLine("Первое число: " + a);
+Console.WriteLine("Второе число: " + b);
+
+if (a > b)
+{
+    Console.WriteLine("Больше первое число: " + a);
+    Console.WriteLine("Меньше второе число: " + b);
+}
+else if (a < b)
+{
+    Console.WriteLine("Меньше первое число: " + a);
+    Console.WriteLine("Больше второе число: " + b);
+}
+else
+{
+    Console.WriteLine($"Первое число: {a} и второе число: {b} равны.");
+}
+
