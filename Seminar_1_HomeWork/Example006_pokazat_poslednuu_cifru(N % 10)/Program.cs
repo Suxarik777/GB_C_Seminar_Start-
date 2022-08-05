284290b9d497fc690f8255262b4a437a956c066f
Suxarik777 <gultcev@mail.ru>
2022-08-06 00:33:53 +0300

    number % 10 поиск последней цифры в трёхзначном числе

---
+++ Seminar_1_HomeWork/Example006_pokazat_poslednuu_cifru(N % 10)/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Дано трёхзначное число: {number}");
+
+int digitLast = number % 10; //поиск последней цифры в трёхзначном числе
+
+Console.WriteLine($"Последняя цифра: {digitLast}");
