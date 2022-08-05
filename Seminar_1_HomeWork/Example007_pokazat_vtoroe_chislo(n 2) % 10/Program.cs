d5c4781fbd2e34a00897cee80c71e198c23d422b
Suxarik777 <gultcev@mail.ru>
2022-08-06 01:00:28 +0300

    поиск второй цифры в трехзначном числе

---
+++ Seminar_1_HomeWork/Example007_pokazat_vtoroe_chislo(n 2) % 10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Дано трёхзначное число: {number}");
+
+int digitTwo = (number / 10) % 10; // получение средней цифры без остатка 25 / 2 = 10
+
+Console.WriteLine($"Вторая цифра: {digitTwo}");
