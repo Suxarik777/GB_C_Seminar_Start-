df0566be01d6a317b793f7cd31dbfd04d286afaf
Suxarik777 <gultcev@mail.ru>
2022-08-05 18:03:17 +0300

    остаток деления на два

---
+++ Seminar_1_HomeWork/Example004_chetnoe_chislo/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"Число {number}, является чётным");
+}
+else
+{
+    Console.WriteLine($"Число {number}, нечётное");
+}
