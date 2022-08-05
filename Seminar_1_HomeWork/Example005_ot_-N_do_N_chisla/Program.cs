ca0ac0d1416f2e1ed75d8ead5b07be3b6d9e7488
Suxarik777 <gultcev@mail.ru>
2022-08-06 00:05:34 +0300

    показать числа от -N до N Print через цикл While

---
+++ Seminar_1_HomeWork/Example005_ot_-N_do_N_chisla/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать числа от -N до N
+
+Console.Write("Введите отрицательное число: ");
+int numberNegative = int.Parse(Console.ReadLine());
+Console.Write("Введите положительное число: ");
+int numberPositive = int.Parse(Console.ReadLine());
+
+int numberPrint = numberNegative; //с этой переменной легче читать код
+while (numberPrint <= numberPositive)
+{
+    Console.Write(numberPrint + " ");
+    numberPrint++;
+}
+Console.WriteLine(" ");
+
+
+
