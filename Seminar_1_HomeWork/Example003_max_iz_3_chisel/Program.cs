ab3909d95400627f04bc842220a46f2b629ccdd8
Suxarik777 <gultcev@mail.ru>
2022-08-05 17:43:57 +0300

    найти наибольшее из трёх чисел

---
+++ Seminar_1_HomeWork/Example003_max_iz_3_chisel/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Смотрим три числа");
+
+int number1 = new Random().Next(1, 10);
+Console.WriteLine($"Первое число: {number1}");
+int number2 = new Random().Next(1, 10);
+Console.WriteLine($"Второе число: {number2}");
+int number3 = new Random().Next(1, 10);
+Console.WriteLine($"Третье число: {number3}");  
+
+int max = 0;
+
+if(max < number1)
+{
+    max = number1;
+}
+else if(max < number2)
+{
+    max = number2;
+}
+else if(max < number3)
+{
+    max = number3;
+}
+
+Console.WriteLine($"Наибольшее число: {max}");
+
