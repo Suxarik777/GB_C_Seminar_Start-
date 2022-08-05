105d052cf0d2db56e8b9038c8868f1a894c411f5
Suxarik777 <gultcev@mail.ru>
2022-08-05 16:27:02 +0300

    Первая задача, проверка квадрата числа

---
+++ Seminar_1_HomeWork/Example001_Proverka_kvadrata_chisla/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.Write("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+if(numberTwo * numberTwo == numberOne)
+{
+    Console.Write($"Олично! {numberOne} является квадратом второго числа {numberTwo}");
+}
+else
+{
+    Console.Write($"Нет! {numberOne} не является квадратом второго числа {numberTwo}");
+}
