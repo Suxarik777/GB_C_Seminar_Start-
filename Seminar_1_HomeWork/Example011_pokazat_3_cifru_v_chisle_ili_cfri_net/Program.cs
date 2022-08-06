e473be0e2d527bbdb1037f7f075c1c73ee005481
Suxarik777 <gultcev@mail.ru>
2022-08-06 19:21:04 +0300

    поиск стретьей цифры через string

---
+++ Seminar_1_HomeWork/Example011_pokazat_3_cifru_v_chisle_ili_cfri_net/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 99)
+{
+    Console.WriteLine("Третья цифра в числе: " + number.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("В числе нет третьей цифры");
+}
