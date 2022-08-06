849f67215aa319f23dbede96e9148d6ddfff3286
Suxarik777 <gultcev@mail.ru>
2022-08-06 15:52:41 +0300

    узнать делится ли число без остатка, если нет то показать остаток

---
+++ Seminar_1_HomeWork/Example010_delenie_dvuh_chisel_s_ostatkom/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+// То есть делится ли ваше число нацело на заданое.
+
+double numberRandom = new Random().Next(1, 10);
+Console.WriteLine($"Дано число: {numberRandom}");
+
+Console.Write("Введите своё число: ");
+double numberUser = double.Parse(Console.ReadLine());
+
+if (numberUser % numberRandom == 0)
+{
+    Console.WriteLine("Ваше число кратно заданому");
+}
+else
+{
+    Console.WriteLine("Ваше число не кратно второму");
+    Console.WriteLine("Остаток от деления " + numberUser % numberRandom);
+}
