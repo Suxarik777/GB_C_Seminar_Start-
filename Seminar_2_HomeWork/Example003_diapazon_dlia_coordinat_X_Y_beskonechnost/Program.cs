9fe8554dc0d4685c9f9f7fa0b384abbd1dc999db
Suxarik777 <gultcev@mail.ru>
2022-08-09 15:27:02 +0300

    отображение позитивной и негативной бесконечности в системе координат X и Y по номеру четверти

---
+++ Seminar_2_HomeWork/Example003_diapazon_dlia_coordinat_X_Y_beskonechnost/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("Введите номер четверти, по отношению к осям X & Y: ");
+int numberQuarter = int.Parse(Console.ReadLine());
+
+double positive = double.PositiveInfinity; //записываем в переменную бесконечность
+double negative = double.NegativeInfinity;
+
+if(numberQuarter == 1)
+{
+    Console.WriteLine($"В четверти {numberQuarter}");
+    Console.WriteLine($"ось X от -1 до {negative}");
+    Console.WriteLine($"ось Y от 1 до {positive}");
+}
+else if
+(numberQuarter == 2)
+{
+    Console.WriteLine($"В четверти {numberQuarter}");
+    Console.WriteLine($"ось X от 1 до {positive}");
+    Console.WriteLine($"ось Y от 1 до {positive}");
+}
+else if
+(numberQuarter == 3)
+{
+    Console.WriteLine($"В четверти {numberQuarter}");
+    Console.WriteLine($"ось X от 1 до {positive}");
+    Console.WriteLine($"ось Y от -1 до {negative}");
+}
+else if
+(numberQuarter == 4)
+{
+    Console.WriteLine($"В четверти {numberQuarter}");
+    Console.WriteLine($"ось X от -1 до {negative}");
+    Console.WriteLine($"ось Y от -1 до {negative}");
+}
+else
+{
+    Console.WriteLine("Упс! Что-то пошло не так!");
+    Console.WriteLine("Попробуйте ввести числа от 1 до 4");
+}
