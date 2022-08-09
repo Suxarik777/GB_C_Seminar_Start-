7d506eda956863eb4d5e21cab1ec26d2e2439b08
Suxarik777 <gultcev@mail.ru>
2022-08-09 14:07:09 +0300

    &&, ||, сложные условия, указать является ли номер дня недели выходным

---
+++ Seminar_2_HomeWork/Example001_slognie_uslovia_if_else_number_day_wekend/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.Write("Укажите номер дня недели: ");
+int numberDay = int.Parse(Console.ReadLine());
+
+if(numberDay >= 1 && numberDay <= 5)
+{
+    Console.WriteLine("Это не выходной. Продолжайте работать ;)");
+}
+else if(numberDay >= 6 && numberDay <= 7)
+{
+    Console.WriteLine("Да! Вы ввели номер выходного дня. Пойдём отдыхать!");
+}
+else
+{
+    Console.WriteLine("УПС! Что то пошло не так. Попробуйте ввести числа от 1 до 7");
+}
