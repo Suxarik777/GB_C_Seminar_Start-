0fa6762261e4defb01cad385a6d16ba3c971de42
Suxarik777 <gultcev@mail.ru>
2022-08-14 00:45:52 +0300

    сумма положительных отрицательных элементов + пользовательский запрос на выполнение с проверкой

---
+++ Seminar_3_HomeWork/Example002_sum_positiv_negativ_i_massiva_+_userRequest/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. 
+// Найти сумму положительных/отрицательных элементов массива
+
+int[] CreatArray(int size)
+{
+    var array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var element in array)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+int SumElementAr(int[] array, string sign)
+{
+    int sum = 0;
+    if (sign == "+")
+    {
+        foreach (var element in array)
+            if (element > 0) sum += element;
+    }
+    else if (sign == "-")
+    {
+        foreach (var element in array)
+            if (element < 0) sum += element;
+    }
+    return sum;
+}
+
+void PrintResult(int sum, string sign)
+{
+    if (sign == "+")
+        Console.WriteLine($"Сумма положительных элементов массива:  {sum}");
+    else if(sign == "-")
+        Console.WriteLine($"Сумма отрицательных элементов массива:  {sum}");
+    else
+      Console.WriteLine($"User Error. Please repeat the request!");  
+}
+
+
+int sizeInArray = 12;
+int[] numberAr = CreatArray(sizeInArray);
+
+Console.WriteLine($"Массив из {sizeInArray} элементов:");
+PrintArray(numberAr);
+
+Console.WriteLine("Укажите сумму положительных или отрицательных "
+                + "элементов массива мы будем искать?");
+Console.Write("Введите + или -: ");
+string userRequest = Console.ReadLine();
+
+int result = SumElementAr(numberAr, userRequest); //(массив и параметр поиска элементов)
+PrintResult(sum: result, sign: userRequest);
