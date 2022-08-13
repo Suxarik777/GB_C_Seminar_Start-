b9ee26ef061102e28278d889e83938ebdf09b547
Suxarik777 <gultcev@mail.ru>
2022-08-13 23:06:45 +0300

    цикл foreach, тип данных var, создание массива

---
+++ Seminar_3_HomeWork/Example001_new_array/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+int[] CreatArray(int size)
+{
+    var array = new int[size];
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+int sizeArray = 8;
+int[] numbers = CreatArray(sizeArray);
+
+Console.WriteLine("Массив из 8 элементов:");
+PrintArray(numbers);
