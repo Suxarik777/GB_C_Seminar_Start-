2d40a9a0d461dfa7b69ab886fbbfbc25b5a7e9ab
Suxarik777 <gultcev@mail.ru>
2022-08-15 00:30:21 +0300

    замена числе в массиве. Умножить на -1

---
+++ Seminar_3_HomeWork/Example003_zamena_elementov_*-1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+int[] CreatArrayRandom(int size)
+{
+    var array = new int [size];
+    var random = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-9, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+int[] Replacing(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+Console.Write("Укажите длину массива: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[] randomNumbers = CreatArrayRandom(length);
+
+Console.WriteLine("Начальная версия массива: ");
+PrintArray(randomNumbers);
+
+int [] randomNumbers2 = Replacing(randomNumbers);
+Console.WriteLine("Массив с заменой элементов на противоположенные: ");
+PrintArray(randomNumbers2);
