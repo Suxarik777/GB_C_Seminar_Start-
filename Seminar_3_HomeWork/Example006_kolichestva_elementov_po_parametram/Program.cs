a6426e1850107ee6bb3d21f1ffc968bd3b212849
Suxarik777 <gultcev@mail.ru>
2022-08-15 03:40:10 +0300

    количество элементов в массиве в отрезке

---
+++ Seminar_3_HomeWork/Example006_kolichestva_elementov_po_parametram/Program.cs
@@ -0,0 +1,42 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] CreatArray()
+{
+    int size = 123;
+    var array = new int[size];
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(0, 1000);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var element in array)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+int QuantityNumbers(int[] array)
+{
+    int quantity = 0;
+    foreach(var element in array)
+        if(element >= 10 && element <= 99) quantity++;
+    return quantity;
+}
+
+bool AreNumbers(int how)
+{
+    if(how > 0) return true;
+    else return false;
+}
+
+int[] numbers = CreatArray();
+Console.WriteLine("Массив вашему вниманию");
+PrintArray(numbers);
+
+int sumDigits = QuantityNumbers(numbers);
+
+bool chekPrint = AreNumbers(sumDigits);
+Console.WriteLine(chekPrint ? $"Кол-во элементов в отрезке [10, 99]: {sumDigits}" 
+                : "Нет чисел  в отрезке [10, 99]");
