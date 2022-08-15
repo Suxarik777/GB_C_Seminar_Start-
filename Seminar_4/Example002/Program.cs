de9bb1ca42f28e9eff7fe29205930b895a5004c9
Suxarik777 <gultcev@mail.ru>
2022-08-15 23:52:06 +0300

    треугольник? 10 в двоичное, вывод чисел фибоначчи от 1 до n

---
+++ Seminar_4/Example002/Program.cs
@@ -0,0 +1,46 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+// int[] DecimalBinary(int number)
+// {
+//     int[] result = new int [10];
+//     int dividing = 0;
+//     for(int i = 0; number >= 1; i++)
+//     {
+//         dividing = number % 2; //*10???
+//         result[i] = dividing;
+//         number = number / 2;
+//     }
+//     return result;
+// }
+
+// void PrintAnswer(int[] array)
+// {
+//     foreach(var element in array)
+//         Console.Write(element + " ");
+// }
+
+
+
+// Console.WriteLine("Введите двухзначное число: ");
+// int number10 = int.Parse(Console.ReadLine() ?? "0");
+
+// int[] answer = DecimalBinary(number10);
+// PrintAnswer(answer);
+
+
+
+int ToBinar(int value){
+    int binar = 0;
+    int i = 0;
+    while(value >= 1){
+        binar = binar + value % 2 * (int)Math.Pow(10, i); //Math.Pow позволяет записать новую цифру слева
+                //binar * 10 + value % 2; // но тут может быть проблема 
+        Console.WriteLine(binar);
+        value = value / 2;
+        i++;
+    }
+    return binar;
+}
+Console.Clear();
+int value = int.Parse(Console.ReadLine());
+Console.WriteLine(ToBinar(value));
---
+++ Seminar_4/Example003/Program.cs
@@ -0,0 +1,52 @@
+﻿// Показать числа Фибоначчи меньшие заданного числа N
+// ВНИМАНИЕ! 3 решение (закоментируй одно)
+
+//Рекурсия
+int Fib(int i)
+{
+    if(i < 1) return 0;
+    if(i == 1) return 1;
+    return Fib(i-1) + Fib(i - 2);
+}
+Console.Clear();
+int N = int.Parse(Console.ReadLine());
+int i = 0;
+int j = 0;
+while( j < N)
+{
+    Console.Write($"{Fib(i)} ");
+    i++;
+    j = Fib(i);
+}
+
+/////////////////////////////////
+
+//просто цикл
+int n = 10946;
+int fibon1 = 0;
+int fibon2 = 1;
+Console.Write("0 1" + " ");
+while (fibon2 <= n - fibon1)
+{
+int temp = fibon2;
+fibon2 = fibon2 + fibon1;
+fibon1 = temp;
+Console.Write(fibon2 + " ");
+}
+
+///////////////////////////////
+
+//через массив
+int[] fibonacci = new int[1000];
+fibonacci[0] = 1;
+fibonacci[1] = 1;
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write($"{fibonacci[0]} {fibonacci[1]} ");
+int index = 1;
+while(fibonacci[index]<n)
+{
+    index++;
+    fibonacci[index]=fibonacci[index-1]+fibonacci[index-2];
+    if(fibonacci[index]<n)
+        Console.Write($"{fibonacci[index]} ");
+}
