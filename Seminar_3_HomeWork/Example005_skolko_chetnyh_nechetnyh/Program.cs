9b6d204bd03a8c674f2ed116421710e1c70af640
Suxarik777 <gultcev@mail.ru>
2022-08-15 03:10:21 +0300

    найти количество четных не четных числе

---
+++ Seminar_3_HomeWork/Example005_skolko_chetnyh_nechetnyh/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. 
+// Показать количество нечетных\четных чисел
+
+int[] CreatArray(int size)
+{
+    var array = new int[size];
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(100, 1000);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var element in array)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+int EvenDigits(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int OddDigits(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Укажите длину массива: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[] numbers = CreatArray(length);
+
+Console.WriteLine("Ваш массив: ");
+PrintArray(numbers);
+
+int evenNumberQsuantity = EvenDigits(numbers);
+int oddNumbersQuantity = OddDigits(numbers);
+
+Console.WriteLine($"В массиве {evenNumberQsuantity} четных и "
+                + $"{oddNumbersQuantity} нечетных числа");
+
