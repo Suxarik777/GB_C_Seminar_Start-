b10e5366b80fd82b83150881b60e19bb57f24d3d
Suxarik777 <gultcev@mail.ru>
2022-08-17 14:27:07 +0300

    поиск пересечения по формуле, а именно поиск x и y

---
+++ Seminar_4_HomeWork/Example001_persechenie_lines_poisk_x_y/Program.cs
@@ -0,0 +1,33 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением 
+// y = k1 * x + b1, y = k2 * x + b2, 
+// b1 k1 и b2 и k2 заданы
+
+double InputNumber(string text)
+{
+    Console.Write(text);
+    return double.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintXY(string text, double xY)
+{
+    Console.Write(text +  xY);
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Укажите координаты ПЕРВОЙ линии.");
+double k1 = InputNumber("Введите координату первой точки -k1-: ");
+double k2 = InputNumber("Введите координату первой точки -k2-: ");
+
+Console.WriteLine();
+
+Console.WriteLine("Укажите координаты ВТОРОЙ линии.");
+double b1 = InputNumber("Введите координату первой точки -b1-: ");
+double b2 = InputNumber("Введите координату первой точки -b2-: ");
+
+double x = (b2 - b1)/(k2-k1);
+double y = k1 * x + b1;
+
+Console.WriteLine("Точка пересечения двух прямых находится");
+PrintXY("По оси X ", x);
+PrintXY("По оси Y ", y);
