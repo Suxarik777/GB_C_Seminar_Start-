b05f211b31c62cbd75740a6f86aa8efb3ca369b7
Suxarik777 <gultcev@mail.ru>
2022-08-19 14:46:23 +0300

    считывание в строку через пробел + олимпиадные задачки

---
+++ Seminar_5/Example002/Program.cs
@@ -0,0 +1,35 @@
+﻿float InputNumber(string text)
+{
+    Console.Write(text);
+    float number = float.Parse(Console.ReadLine());
+    return number;
+}
+
+float x1 = InputNumber("Первый друг X1: ");
+float x2 = InputNumber("Второй друг X2: ");
+float x3 = InputNumber("Третий друг X3: ");
+
+float distance = 0;
+
+if(x2 - x1 == x3 - x2) //проверяем есть ли друг по середине (в данном случае x2)
+{
+    if(x2 - x1 > 0) distance = (x2 - x1) * 2; //условие чтобы не получить отрицательное число
+    else distance = (x1 - x2) * 2; // в противном случае переворачеваем, чтобы получить полож.
+}
+else if (x1 - x3 == x2 - x1)
+{
+    if(x1 - x3 > 0) distance = (x1 - x3) * 2;
+    else distance = (x3 - x1) * 2;
+}
+else if (x3 - x2 == x1 - x3)
+{
+    if(x3 - x2 > 0) distance = (x3 - x2) * 2;
+    else distance = (x2 - x3) * 2;
+    //distance = (x3 - x2) * 2;
+    //(x1 + x2 + x3)/2;
+}
+else
+{
+    distance = (x1 + x2 + x3)/3; //условие если все на разном удалении
+}
+Console.WriteLine(distance);
---
+++ Seminar_5/Example003/Program.cs
@@ -0,0 +1,6 @@
+﻿//Считывание чисел строкой в int через пробел
+
+
+string str = Console.ReadLine();
+        int[] numbers = str.Split(' ').Select(int.Parse).ToArray();
+        Console.WriteLine(numbers[0]+numbers[1]);
