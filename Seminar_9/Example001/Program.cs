4991c08a66c530cfee28e9da7a02b014a71dfcd8
Suxarik777 <gultcev@mail.ru>
2022-09-02 11:23:30 +0300

    рекурсия, от 1 до N, сумма цифр в числе

---
+++ Seminar_9/Example001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+int InputNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+string FromToN (int digit, int n)
+{
+    if (digit <= n) return $"{digit} " + FromToN(digit + 1, n); 
+    else return String.Empty;
+}
+
+int numberN = InputNumber("Введите число N: ");
+int number = 1;
+
+Console.WriteLine(FromToN(number, numberN));
