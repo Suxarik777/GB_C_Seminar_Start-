c6f35f007e210a9522ea08afb3643b8a313c548c
Suxarik777 <gultcev@mail.ru>
2022-09-05 13:31:22 +0300

    создание и вывод треугольника паскаля + рекурсия факториала

---
+++ Seminar_8_HomeWork/Example004_treugolnik_paskalia/Program.cs
@@ -0,0 +1,38 @@
+﻿//Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+int InputNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void MagicPaskale(int side)
+{
+   for (int i = 0; i < side; i++)
+    {
+        for (int c = 0; c <= (side - i); c++) 
+        {
+            Console.Write(" "); 
+        }
+        for (int c = 0; c <= i; c++)
+        {
+            Console.Write(" ");
+            Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); 
+        }
+        Console.WriteLine();
+        Console.WriteLine(); 
+    }
+}
+
+int factorial(int digit)
+{
+    int x = 1;
+    for (int i = 1; i <= digit; i++) 
+        x *= i;
+    return x;
+}
+
+
+int sideTriangle = InputNumber("Укажите длину стороны треугольника: ");
+MagicPaskale(sideTriangle);
