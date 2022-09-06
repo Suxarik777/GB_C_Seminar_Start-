f1d52c549d7aee4bf9f228c767ea2dac7958e201
Suxarik777 <gultcev@mail.ru>
2022-09-06 16:49:18 +0300

    рекурсия A в степень B

---
+++ Seminar_9_HomeWork/Example005_rec_A_v_stepeni_N/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int InputNumberA(string text)
+{
+    Console.Write(text);
+    int numberA = int.Parse(Console.ReadLine());
+    return numberA;
+}
+
+int InputNumberB(string text)
+{
+    Console.Write(text);
+    int numberB = int.Parse(Console.ReadLine());
+    if (numberB > 0) return numberB;
+    else return InputNumberB("Не получится! Нужно указать B больше нуля: ");
+}
+
+
+int RecursionPow(int a, int b)
+{
+    if (b > 0) return a * RecursionPow(a, b - 1);
+    return 1;
+}
+
+int numberA = InputNumberA("Введите число A: ");
+int numberB = InputNumberB("Введите степень B: ");
+
+Console.WriteLine(RecursionPow(numberA, numberB));
