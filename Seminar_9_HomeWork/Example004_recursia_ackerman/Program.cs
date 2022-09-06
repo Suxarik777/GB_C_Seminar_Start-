a28f20bf651dd5c0d3a3c16185f1cce4d6c96e47
Suxarik777 <gultcev@mail.ru>
2022-09-06 16:14:09 +0300

    рекурсия Аккермана

---
+++ Seminar_9_HomeWork/Example004_recursia_ackerman/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int InputNumberM(string text)
+{
+    Console.Write(text);
+    int numberM = int.Parse(Console.ReadLine());
+    if (numberM > 0) return numberM;
+    else return InputNumberM("Не получится! Нужно указать M больше нуля: ");
+}
+
+int InputNumberN(string text, int m)
+{
+    Console.Write(text);
+    int numberN = int.Parse(Console.ReadLine());
+    if (numberN > m) return numberN;
+    else return InputNumberN("Не получится! Нужно указать N, чтобы была больше M: ", m);
+}
+
+int AckermanRecursion(int m, int n)
+{
+    if (m == 0)                 return n + 1;
+    else if (n == 0 && m > 0)   return AckermanRecursion(m - 1, 1);
+    else                        return AckermanRecursion(m - 1, AckermanRecursion(m, n - 1));
+}
+
+int numberM = InputNumberM("Введите число M: ");
+int numberN = InputNumberN("Введите число N: ", numberM);
+
+Console.WriteLine(AckermanRecursion(numberM, numberN));
